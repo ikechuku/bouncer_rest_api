5bbdbf5ff49fde77b3c425af51c519af0fc8daa7
Adams Temidire A <gr8temi@gmail.com>
2019-12-13 11:11:03 +0100

    spread user_data into User create method

--- bouncer/api/tests/user/test_email_verification.py
+++ bouncer/api/tests/user/test_email_verification.py
@@ -7,13 +7,7 @@ from ...models.user.users import User
 class TestEmailVerification(APITestCase):
     
     def test_verify(self):
-        user = User.objects.create(
-            user_name=user_data["user_name"],
-            password = user_data["password"],
-            user_type = user_data["user_type"],
-            email_verified=user_data["email_verified"],
-            token=user_data["token"]
-        )
+        user = User.objects.create(**user_data)
         token = user.token
         url = '/api/auth/verify-email/'
         response =self.client.post(url,{"token":token})
