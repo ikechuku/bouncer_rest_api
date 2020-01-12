dbf0e60b46cc9b271f4fbefa6980e3ee3e312614
ikechuku <rockstarik2@gmail.com>
2020-01-12 03:45:11 +0100

    Add mock data for product

---
+++ bouncer/api/tests/mock_data/product_data.py
@@ -0,0 +1,11 @@
+def product_data():
+    return {
+        "name": "Ipad",
+        "quantity": "2",
+        "price": "250",
+    }
+
+def test_product_data():
+    return {
+        "id" : 1
+    }
